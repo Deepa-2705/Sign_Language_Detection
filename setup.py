9fccb96511944bddca27753b00fe687dfb481148
Deepa-2705 <deepagupta35795@gmail.com>
2024-07-01 15:26:56 +0530

    Sign Language Detection project updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'Deepa Gupta',
+    author_email= 'deepagupta35795@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
